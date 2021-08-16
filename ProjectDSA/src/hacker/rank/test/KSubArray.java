376769da8fb9edb96d32f66570286d9e56902a93
Kamalakannan S <kamalakannanece40@gmail.com>
2021-08-16 19:42:30 +0530

    kSubArray hacker rank solution

--- ProjectDSA/src/hacker/rank/test/KSubArray.java
+++ ProjectDSA/src/hacker/rank/test/KSubArray.java
@@ -2,31 +2,35 @@ package hacker.rank.test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class KSubArray {
-	
+
 	@Test
 	public void test1() {
 		int k = 5;
-		int[] array = {5,10,11,9,5};
-		//5,   5 10,   5 10 11,   5 10 11 9,   5 10 11 9 5,
-		//10,   10 11,   10 11 9,   10 11 9,   10 11 9 5,
-		//11,   11 9,   11 9 5, 
-		//9,   9 5
-		usingSlidingWindow(array,k);
-	}
-
-	private long usingBruteForce(int[] array, int k) {
-	
+		Arrays.asList(new int[] {5,10,11,9,5});
 		List<Integer> ali  = new ArrayList<Integer>();
 		ali.add(5);
 		ali.add(10);
 		ali.add(11);
 		ali.add(9);
 		ali.add(5);
+		//5,   5 10,   5 10 11,   5 10 11 9,   5 10 11 9 5,
+		//10,   10 11,   10 11 9,  10 11 9 5,
+		//11,   11 9,   11 9 5, 
+		//9,   9 5
+		//5
+		Assert.assertEquals(10, kSub(ali, k));
+	}
+
+	private long usingBruteForce(List<Integer> ali, int k) {
+
 		//iterate the list
 		long count = 0;
 		for(int i = 0;i<ali.size();i++) {
@@ -39,44 +43,40 @@ public class KSubArray {
 		System.out.println(count);
 		return count;
 	}
-	private long usingSlidingWindow(int[] arr, int k) {
-		
-		// create auxiliary hash array to
-        // count frequency of remainders
-        int mod[] = new int[k];
-        int  n = arr.length;
-        Arrays.fill(mod, 0);
-        // Traverse original array and compute cumulative
-        // sum take remainder of this current cumulative
-        // sum and increase count by 1 for this remainder
-        // in mod[] array
-        int cumSum = 0;
-        for (int i = 0; i < n; i++) {
-            cumSum += arr[i];
- 
-            // as the sum can be negative, taking modulo twice
-            mod[((cumSum % k) + k) % k]++;
-        }
- 
-        // Initialize result
-        int result = 0;
- 
-        // Traverse mod[]
-        for (int i = 0; i < k; i++) {
- 
-            // If there are more than one prefix subarrays
-            // with a particular mod value.
-            if (mod[i] > 1) result += (mod[i] * (mod[i] - 1)) / 2;
-        }
-        // add the elements which are divisible by k itself
-        // i.e., the elements whose sum = 0
-        result += mod[0];
-        System.out.println(result);
- 
-        return result;
-		
-		
-		
+	public long kSub(List<Integer> nums, int k) {
+
+		// Cumulative Sum of each number in the list
+		int sum = 0;
+
+		// Return output 
+		long output = 0;
+
+		// Find the frequency of remainder occurrence
+		Map<Integer, Integer> freqMap = new HashMap<>();
+
+		// Traverse 
+		for(int i=1;i<=nums.size();++i) {
+
+			// Calculate remainder and add K to it if it is negative
+			int rem = sum%k < 0? sum%k + k : sum%k;
+
+			//  If the remainder exist, update else add
+			freqMap.put(rem, freqMap.getOrDefault(rem, 0)+1);
+
+			// add the previous number to cumulative sum
+			sum +=nums.get(i-1);
+
+			// Calculate remainder and add K to it if it is negative
+			rem = sum%k < 0? sum%k + k : sum%k;
+
+			// If found, add to the output
+			if(freqMap.containsKey(rem)) {
+				output+=freqMap.get(rem);
+			}
+		}
+		System.out.println(output);
+		return output;
+
 	}
 
 }
