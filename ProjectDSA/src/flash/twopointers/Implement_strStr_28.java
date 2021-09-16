c906edc028bc0a01258a5fc30e7a7e3a00073f8a
Kamalakannan S <kamalakannanece40@gmail.com>
2021-09-17 01:49:21 +0530

    implement str is present in another str Jump game 7 usin dequeue

---
+++ ProjectDSA/src/flash/twopointers/Implement_strStr_28.java
@@ -0,0 +1,72 @@
+package flash.twopointers;
+
+import java.util.LinkedList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//https://leetcode.com/problems/implement-strstr/
+public class Implement_strStr_28 {
+
+	@Test
+	public void test1() {
+		String haystack = "mississippi";
+		String needle = "issip";
+		Assert.assertEquals(4, matchingNeedle(haystack,needle));
+	}
+
+	@Test
+	public void test2() {
+		String haystack = "aaaaa";
+		String needle = "bba";
+		Assert.assertEquals(-1, matchingNeedle(haystack,needle));
+	}
+	
+	@Test
+	public void test3() {
+		String haystack = "aaa";
+		String needle = "aaaa";
+		Assert.assertEquals(-1, matchingNeedle(haystack,needle));
+	}
+
+	//declare 2 pointers i at haystack start and j at start of needle
+	//check if the char at i and j are equal increment i and j
+	//  yes-> int temp = i
+	//iterate the size of needle say k
+	//check value at temp not equal to k break
+	//output of the loop return i
+	//if the char are not matching and j is at 0 index increment i
+	//if the char are not matching and j is not at 0 index 
+
+	private int matchingNeedle(String haystack, String needle) {
+		if(needle.length() == 0) return 0;
+		int i = 0, j = 0;
+		while(i<haystack.length()){
+			if(haystack.charAt(i) == needle.charAt(j)) {
+				int temp = i+1, k = 1;
+				while(k<needle.length() && temp<haystack.length()) {
+					if(haystack.charAt(temp++) == needle.charAt(k)) k++;
+				}
+				if(k == needle.length()) return i;
+				//reset j
+				//j = 0;
+			}
+			i++;
+		}
+		return -1;
+	}
+	
+	private int matchingNeedleOptimized(String haystack, String needle) {
+		if(needle.length() == 0) return 0;
+		return haystack.indexOf(needle);
+	}
+	
+	 public int strStr(String haystack, String needle) {
+	       //get the size of needle
+	        int windowSize = needle.length();
+	        for(int i =0;i<haystack.length()-windowSize+1;i++){
+	            if(haystack.substring(i,i+windowSize).equals(needle)) return i;
+	        }
+	        return -1;
+	    }
+}
---
+++ ProjectDSA/src/flash/twopointers/JumpGame7_1871.java
@@ -0,0 +1,62 @@
+package flash.twopointers;
+
+import java.awt.image.IndexColorModel;
+import java.util.ArrayDeque;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//https://leetcode.com/problems/jump-game-vii/
+public class JumpGame7_1871 {
+
+	@Test
+	public void test1() {
+		String s = "011010";
+		int minJump = 2;
+		int maxJump = 3;
+		Assert.assertTrue(jumpGame(s, minJump,maxJump));
+	}
+	
+	@Test
+	public void test2() {
+		String s = "01101110";
+		int minJump = 2;
+		int maxJump = 3;
+		Assert.assertFalse(jumpGame(s, minJump,maxJump));
+	}
+
+	/*using queue
+	 * declare a queue of int
+	 * declare an maxIndex int of 0
+	 * add 0 to queue
+	 * if the queue is not empty loop
+	 * poll the element from queue and keep in a variable i
+	 * check if i is last index yes->return true
+	 * get the i+minJump as min
+	 * get the i+maxJump as max
+	 * declare start index as max of min and maxIndex
+	 * traverse from min to max or min to length of string say index k
+	 * 	check if value at K is 0
+	 * 	add the value to queue
+	 * get the maxIndex as max of max and maxIndex.
+	 */
+	
+	private boolean jumpGame(String s, int minJump, int maxJump) {
+		if(s.charAt(s.length()-1) == '1') return false;
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		int maxIndex = 0;
+		q.offer(0);
+		while(!q.isEmpty()) {
+			int i = q.remove();
+			if(i == s.length()-1) return true;
+			int min = i+minJump;
+			int max = i+maxJump;
+			int start = Math.max(min, maxIndex);
+			for(int j = start;j<=max && j<s.length();j++) {
+				if(s.charAt(j) == '0') q.offer(j);
+			}
+			maxIndex = Math.max(maxIndex, max);
+		}
+		return false;
+	}
+}
