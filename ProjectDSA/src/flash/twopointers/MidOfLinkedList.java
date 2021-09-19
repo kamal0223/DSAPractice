f41f38cef207f36befe061c921f6b3811fb73725
Kamalakannan S <kamalakannanece40@gmail.com>
2021-09-20 01:24:21 +0530

    Two pointer and stack problems

---
+++ ProjectDSA/src/flash/twopointers/MidOfLinkedList.java
@@ -0,0 +1,72 @@
+package flash.twopointers;
+
+import org.junit.Test;
+
+import LinkedList.CountNodeLinkedList.ListNode;
+
+public class MidOfLinkedList {
+	
+	@Test
+	public void test1() {
+		ListNode head = new ListNode(11);
+        head.next = new ListNode(21);
+        head.next.next = new ListNode(23);
+        head.next.next.next = new ListNode(40);
+        printAllNodes(middleNode(head));
+	}
+	
+	@Test
+	public void test2() {
+		ListNode head = new ListNode(1);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(0);
+        head.next.next.next.next = new ListNode(7);
+        System.out.println();
+        printAllNodes(middleNode(head));
+	}
+
+	private void printAllNodes(ListNode head) {
+		while(head!=null) {
+			System.out.println(head.val);
+			head = head.next;
+		}
+		
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode() {}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+	
+	
+	public ListNode middleNode(ListNode head) {
+        //to find the mid node for odd number of list
+        //to find mid+1 node for even number of list
+        //create two pointers right as head and left as head
+        //right is the fast pointer
+        //iterate till right.next becomes null
+        //mid = get the value at right /2 and add 1 
+        //move left till it reaches mid value
+        //return left
+        
+        ListNode left = head;
+        ListNode right = head;
+        int leftIndex = 1;
+        int rightIndex = 1;
+        while(right != null){
+            int mid = (rightIndex)/2+1;
+            while(leftIndex < mid) {
+                left = left.next;
+                leftIndex++;
+            }
+            right = right.next;
+            rightIndex++;
+        }
+        
+        return left;
+    }
+}
---
+++ ProjectDSA/src/flash/twopointers/MinSwapsForBalanceBrackets.java
@@ -0,0 +1,66 @@
+package flash.twopointers;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MinSwapsForBalanceBrackets {
+
+	@Test
+	public void test1() {
+		String s = "][][";
+		Assert.assertEquals(1, minSwapOptimized(s));
+	}
+
+	@Test
+	public void test2() {
+		String s = "]]][[[";
+		Assert.assertEquals(2, minSwapOptimized(s));
+	}
+
+	@Test
+	public void test3() {
+		String s = "[]";
+		Assert.assertEquals(0, minSwapOptimized(s));
+	}
+
+	private int minSwaps(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for(int i = 0;i<s.length();i++){
+			if(!stack.isEmpty() && s.charAt(i) == ']' && stack.peek() == '['){
+				stack.pop();   
+			}
+			else stack.push(s.charAt(i));
+		}
+		int size = stack.size();
+		return ((size/2)+1)/2;
+	}
+
+	public int minSwapOptimized(String s) {
+
+		//the count of [ and ] bracets are are equal
+		//at a given point of time the string is unbalanced when ] count becomes more than [
+		//When an [ is encountered increment balance and on ] decrement balance
+		//when the balance becomes -1 then swap it with the opening first [ bracket from the last and make balance 1
+		//continue this process till all character in string
+		int balance = 0;
+		int swap = 0;
+		char[] charArr = s.toCharArray();
+		for(int i = 0;i<s.length();i++){
+			if(charArr[i] == '[') balance++;
+			else balance--;
+			if(balance == -1){
+				int lastIndex = s.lastIndexOf('[');
+				char temp = charArr[lastIndex];
+				charArr[lastIndex] = charArr[i];
+				charArr[i] = temp;
+				balance = 1;
+				swap++;
+			}          
+		}
+		return swap;
+	}
+
+}
